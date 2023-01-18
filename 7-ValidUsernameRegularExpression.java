1d1f37afe4abbc85f7e52c0ba8556dbf96d229ac
anapbds <justtley@gmail.com>
2023-01-18 19:42:57 -0300

    Desafio 7 - finalizado

---
+++ 7-ValidUsernameRegularExpression.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class UsernameValidator {
+    
+    // [a-zA-Z][a-zA-Z0-9_]{7,29}
+    // \\w | '_' {7,29}
+    String username = 
+    
+    public static final String regularExpression = "^[a-zA-Z][a-zA-Z0-9_]{7,29}$";
+}
+
+
+public class Solution {
+    private static final Scanner scan = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }           
+        }
+    }
+}
